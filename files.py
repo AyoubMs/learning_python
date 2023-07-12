57ccc3779ef5607bae8bcd0d3c04e65fbb38ce16
AyoubMS <chahayb563@gmail.com>
2023-07-12 17:47:07 +0200

    add all files

---
+++ files.py
@@ -0,0 +1,14 @@
+with open('xmen.txt', 'w+t') as my_file:
+    my_file.write('Beast\n')
+    my_file.write('Phoenix\n')
+    my_file.writelines([
+        'Cyclops\n',
+        'Bishop\n',
+        'Nightcrawler\n'
+    ])
+
+
+with my_file:
+    print(my_file.read())
+    print("I'm reading again")
+    print(my_file.read())
